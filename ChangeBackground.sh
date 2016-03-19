d59945c52d60857d135a3b26222642380b849937
Fox <adrian.cieluch@gmail.com>
2016-03-19 15:36:13 +0100

    Added script to change background using nitrogen for ubuntu and i3wm

---
+++ ChangeBackground.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$(id -u)" -ne "0" ]; then
+	echo "This script must be run as root" 1>&2
+	exit 0;
+fi
+
+if [ $# = 0 ]; then
+	echo "Usage: $0 path/to/image"
+	exit 0;
+fi
+
+if [ -f $1 ]; then
+	cp $1 /usr/share/backgrounds/background.jpg
+	nitrogen --set-centered --save /usr/share/backgrounds/background.jpg
+	update-grub
+fi
+
+exit 0;
